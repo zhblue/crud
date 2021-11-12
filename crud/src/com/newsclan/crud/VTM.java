b19d4e8f7be8240c00e6dd3ffef9929f7c1ae5ad
张浩斌 <newsclan@gmail.com>
2021-11-12 14:28:33 +0800

    Update VTM.java

--- crud/src/com/newsclan/crud/VTM.java
+++ crud/src/com/newsclan/crud/VTM.java
@@ -1,24 +1,101 @@
 package com.newsclan.crud;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringBufferInputStream;
-import java.io.StringReader;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.util.zip.*;
 
 public class VTM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String filepath = "demo.srt";
-		readSrtFile(filepath);
+//		String filepath = "demo.srt";
+//		readSrtFile(filepath);
+
+		String sql = "select c.id,c.tag_name,m.movie_name,c.start_time,c.end_time,c.clip_file from tb_clips c inner join tb_movie m on c.tb_movie_id=m.id";
+		exportClipsBySQL(sql);
+		System.exit(0);
+	}
+
+	public static void ZipOneFile(String target, String source) {
+		FileOutputStream outputStream = null;
+		ZipOutputStream zipOutputStream = null;
+		FileInputStream input = null;
+		try {
+			outputStream = new FileOutputStream(target);
+			zipOutputStream = new ZipOutputStream(new BufferedOutputStream(outputStream));
+			input = new FileInputStream(source);
+
+			zipOutputStream.putNextEntry(new ZipEntry(source));
+
+			int readLen = 0;
+			byte[] buffer = new byte[1024 * 8];
+			while ((readLen = input.read(buffer, 0, 1024 * 8)) != -1) {
+				zipOutputStream.write(buffer, 0, readLen);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			if (zipOutputStream != null)
+				zipOutputStream.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			if (input != null)
+				input.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void ZipFiles(String target, String sources[]) {
+
+		FileOutputStream outputStream = null;
+		ZipOutputStream zipOutputStream = null;
+		FileInputStream input = null;
+		try {
+			outputStream = new FileOutputStream(target);
+			zipOutputStream = new ZipOutputStream(new BufferedOutputStream(outputStream));
+
+			for (String source : sources) {
+
+				try {
+					input = new FileInputStream(source);
+					zipOutputStream.putNextEntry(new ZipEntry(source));
+					int readLen = 0;
+					byte[] buffer = new byte[1 << 20];
+					while ((readLen = input.read(buffer, 0, 1024 * 8)) != -1) {
+						zipOutputStream.write(buffer, 0, readLen);
+					}
+					input.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			zipOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			if (zipOutputStream != null)
+				zipOutputStream.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			if (input != null)
+				input.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static void readSrtFile(String filepath) {
@@ -125,7 +202,8 @@ public class VTM {
 				build_task_id);
 		List task = DAO.queryList("select tb_movie_id,fastsave from tb_manual_task where id=?", false, manual_task_id)
 				.get(0);
-		final String movie_path = DAO.queryString("select movie_file from tb_movie where id=?", task.get(0));
+		final String movie_id = String.valueOf(task.get(0));
+		final String movie_path = DAO.queryString("select movie_file from tb_movie where id=?", movie_id);
 		String srt = (String) task.get(1);
 		final List<String[]> subtasks = parseSRT(srt);
 		if (subtasks.size() > 0)
@@ -136,60 +214,67 @@ public class VTM {
 				@Override
 				public void run() {
 					// TODO Auto-generated method stub
-					int i=0;
+					int i = 0;
+					String start_time, end_time;
 					for (String[] subtask : subtasks) {
 
 						String[] envp = new String[] {};
 						String[] cmdarray = new String[3];
-						System.out.println("movie_path:"+movie_path);
+						System.out.println("movie_path:" + movie_path);
 						String extense = movie_path.substring(movie_path.length() - 4);
 						String output = Config.path + "output/" + build_task_id;
-						System.out.println("Output:"+output);
+						System.out.println("Output:" + output);
 						File dir = new File(output);
 						if (!dir.exists())
 							dir.mkdirs();
-						String tagText= subtask[3];
-						output += "/" +tagText+"_"+i + extense;
+						String tagText = subtask[3];
+						output += "/" + tagText + "_" + i + extense;
 						cmdarray[0] = "/bin/bash";
-						cmdarray[1] ="-c";
-						cmdarray[2] = Config.path+"WEB-INF/cut.sh";
-						cmdarray[2] +=" "+Config.path+ movie_path; // input
-						cmdarray[2] +=" "+ subtask[1].replace(',', '.'); // start
-						cmdarray[2] +=" "+ subtask[2].replace(',', '.'); // end
-						cmdarray[2] +=" "+ output; // tagText
-						executeCMD( cmdarray,envp, dir);
+						cmdarray[1] = "-c";
+						cmdarray[2] = Config.path + "WEB-INF/cut.sh";
+						cmdarray[2] += " " + Config.path + movie_path; // input
+						start_time = subtask[1].replace(',', '.');
+						end_time = subtask[2].replace(',', '.');
+						cmdarray[2] += " " + start_time; // start
+						cmdarray[2] += " " + end_time; // end
+						cmdarray[2] += " " + output; // tagText
+						executeCMD(cmdarray, envp, dir);
 						i++;
-						List<List>already=DAO.queryList("select id from tb_clips where clip_file=?",false, output.substring(Config.path.length()));
-						if(already.size()>0)
-							DAO.executeUpdate("update tb_clips set tag_name=?,update_time=now(),tb_movie_id=?,start_time=?,end_time=? where id=?",
-							tagText,movie_id,start_time,end_time,already.get(0).get(0).toString());
+						List<List> already = DAO.queryList("select id from tb_clips where clip_file=?", false,
+								output.substring(Config.path.length()));
+						if (already.size() > 0)
+							DAO.update(
+									"update tb_clips set tag_name=?,update_time=now(),tb_movie_id=?,start_time=?,end_time=? where id=?",
+									tagText, movie_id, start_time, end_time, already.get(0).get(0).toString());
 						else
-							DAO.update("insert into tb_clips(tag_name,update_time,tb_movie_id,start_time,end_time,clip_file)"
-									+ " values(?,now(),?,?,?,?)",tagText,movie_id,start_time,end_time,output.substring(Config.path.length()));
-					
-						DAO.update("update tb_build_task set finished_task_num=? where id=?",i,build_task_id);
-						DAO.update("update tb_build_task set percent=finished_task_num*100/sub_task_num where id=?",build_task_id);
-						
+							DAO.update(
+									"insert into tb_clips(tag_name,update_time,tb_movie_id,start_time,end_time,clip_file)"
+											+ " values(?,now(),?,?,?,?)",
+									tagText, movie_id, start_time, end_time, output.substring(Config.path.length()));
+						DAO.update("update tb_build_task set finished_task_num=? where id=?", i, build_task_id);
+						DAO.update("update tb_build_task set percent=finished_task_num*100/sub_task_num where id=?",
+								build_task_id);
+
 					}
-					
+
 				}
 
-				public void executeCMD(String[] cmdarray,String[] envp, File dir) {
-					System.out.format("%s %s %s\n",cmdarray[0],cmdarray[1],cmdarray[2]);
+				public void executeCMD(String[] cmdarray, String[] envp, File dir) {
+					System.out.format("%s %s %s\n", cmdarray[0], cmdarray[1], cmdarray[2]);
 					try {
-						Process proc=Runtime.getRuntime().exec(cmdarray, envp, dir);
+						Process proc = Runtime.getRuntime().exec(cmdarray, envp, dir);
 						InputStream stdin = proc.getInputStream();
-					    InputStreamReader isr = new InputStreamReader(stdin);
-					
-					        BufferedReader br = new BufferedReader(isr);
-					        String line = null;
-					     
-					        while ((line = br.readLine()) != null)
-					            System.out.println(line);
-					        System.out.println("");
-					        int exitVal = proc.waitFor();
+						InputStreamReader isr = new InputStreamReader(stdin);
+
+						BufferedReader br = new BufferedReader(isr);
+						String line = null;
+
+						while ((line = br.readLine()) != null)
+							System.out.println(line);
+						System.out.println("");
+						int exitVal = proc.waitFor();
 						System.out.println("Process exitValue: " + exitVal);
-						
+
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -207,5 +292,104 @@ public class VTM {
 		}
 
 		return "任务异常！";
+	}
+
+	public static void exportClipsBySQL(String sql) {
+		List<List> data = DAO.queryList(sql, true);
+		List title = data.get(0);
+		String indexFile = Config.path + "output/" + Tools.today();
+		File dir=new File(indexFile);
+		if(!dir.exists()) dir.mkdirs();
+		final String target=indexFile+"/clips_"+Tools.getRandomSalt()+".zip";
+		indexFile+= "/index_" + Tools.getRandomSalt() + ".xml";
+		final String [] sources=new String[data.size()];
+		try {
+			PrintStream out=new PrintStream(indexFile,"UTF-8");
+			sources[0]=indexFile;
+			System.out.println(indexFile);
+			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			out.println("<clips>");
+			for (int i = 1; i < data.size(); i++) {
+				out.println("<clip>");
+				List row = data.get(i);
+				for (int j = 0; j < title.size(); j++) {
+					out.print("<"+title.get(j)+">");
+					out.print(row.get(j));
+					out.println("</"+title.get(j)+">");
+					if(((String)(title.get(j))).contains("文件")){
+						sources[i]=(String) row.get(j);
+					}
+				}
+				out.println("</clip>");
+			}
+			out.println("</clips>");
+			
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				FileOutputStream outputStream = null;
+				ZipOutputStream zipOutputStream = null;
+				FileInputStream input = null;
+				try {
+					outputStream = new FileOutputStream(target);
+					zipOutputStream = new ZipOutputStream(new BufferedOutputStream(outputStream));
+					boolean first=true;
+					for (String source : sources) {
+
+						try {
+							
+							if(first) {
+								input = new FileInputStream(source);
+								zipOutputStream.putNextEntry(new ZipEntry("index.xml"));
+								first=false;
+							}
+							else {
+								input = new FileInputStream(Config.path+source);
+								zipOutputStream.putNextEntry(new ZipEntry(source));
+							}
+							int readLen = 0;
+							byte[] buffer = new byte[1 << 20];
+							while ((readLen = input.read(buffer, 0, 1024 * 8)) != -1) {
+								zipOutputStream.write(buffer, 0, readLen);
+							}
+							input.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+					zipOutputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				try {
+					if (zipOutputStream != null)
+						zipOutputStream.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					if (input != null)
+						input.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}).start();
+		
 	}
 }
