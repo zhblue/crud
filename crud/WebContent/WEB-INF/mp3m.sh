cacab47dac7c1549f280f495fd0bf75670c13c60
张浩斌 <newsclan@gmail.com>
2023-11-19 21:06:21 +0800

    Create mp3m.sh

---
+++ crud/WebContent/WEB-INF/mp3m.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+WAV="$1"
+MP3="$2"
+FFMPEG=`/usr/bin/which ffmpeg`
+$FFMPEG -i "$WAV"  -acodec libmp3lame -af "volume=23dB" -y "$MP3" || exit 1
+if test "$WAV" -ot "$MP3" ;then rm "$WAV";fi
