a0fd3e46b5afdb27d1490620550b4452b527a0bb
张浩斌 <newsclan@gmail.com>
2023-11-21 15:26:19 +0800

    Add files via upload

--- crud/WebContent/WEB-INF/mp3m.sh
+++ crud/WebContent/WEB-INF/mp3m.sh
@@ -2,5 +2,5 @@
 WAV="$1"
 MP3="$2"
 FFMPEG=`/usr/bin/which ffmpeg`
-$FFMPEG -i "$WAV"  -acodec libmp3lame -y "$MP3" || exit 1
+$FFMPEG -i "$WAV"  -acodec libmp3lame -af "volume=18dB" -y "$MP3" || exit 1
 if test "$WAV" -ot "$MP3" ;then rm "$WAV";fi
