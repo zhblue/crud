ba9d7714a0cf8798e37ff017011835b4608a87e4
张浩斌 <newsclan@gmail.com>
2023-11-24 09:29:13 +0800

    Update attach.sh

--- crud/WebContent/WEB-INF/attach.sh
+++ crud/WebContent/WEB-INF/attach.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 FFMPEG=`/usr/bin/which ffmpeg`
 $FFMPEG $*
+OUTFILE=${@: -1}
+TEMPFILE=`dirname $OUTFILE`"/"`basename $OUTFILE`"_$RANDOM"
+mv "$OUTFILE" "$TEMPFILE"
+$FFMPEG -i "$TEMPFILE" -af "volume=12dB" $OUTFILE
+rm "$TEMPFILE"
